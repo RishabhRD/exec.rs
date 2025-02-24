41453c4e9506df62ffb900139933a19b94b0985d
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-02-24 12:30:02 +0000

    added implementation of inline_scheduler

--- src/core.rs
+++ src/core.rs
@@ -17,12 +17,17 @@
 /// Represents executable task.
 pub trait OperationState {
     /// Start the task represented by operation state.
+    ///
+    /// # Precondition
+    ///   - Called no more than 1 time.
     fn start(&mut self);
 }
 
 /// This models the part of task which contains callback when task completes.
 ///
 /// A task can be completed with Value, Error or cancelled.
+/// One of the set_value, set_error, set_cancelled should be called only
+/// single time.
 pub trait Receiver {
     /// Type of value with which task can be completed.
     type Value;
@@ -31,13 +36,13 @@ pub trait Receiver {
     type Error;
 
     /// Callback for task, when it completes with value.
-    fn set_value(self, value: Self::Value);
+    fn set_value(&mut self, value: Self::Value);
 
     /// Callback for task, when it completes with error.
-    fn set_error(self, error: Self::Error);
+    fn set_error(&mut self, error: Self::Error);
 
     /// Callback for task, when it gets cancelled.
-    fn set_cancelled(self);
+    fn set_cancelled(&mut self);
 }
 
 /// This models the part of task which represents the work upon completion of
@@ -49,21 +54,15 @@ pub trait Sender {
     /// Type of error with which task can be completed.
     type Error;
 
-    /// Type of operation state when Sender is connected with compatible receiver.
-    type OperationState;
-
     /// Connects sender with receiver to produce executable task represented
     /// by operation state.
-    fn connect<R>(self, receiver: R) -> Self::OperationState
+    fn connect<R>(self, receiver: R) -> impl OperationState
     where
         R: Receiver<Value = Self::Value, Error = Self::Error>;
 }
 
 /// Represents handle to execution context.
 pub trait Scheduler {
-    /// Type of sender obtained from scheduling a task on scheduler.
-    type Sender;
-
     /// Returns a sender that represent task scheduled on scheduler.
-    fn schedule(&mut self) -> Self::Sender;
+    fn schedule(&mut self) -> impl Sender;
 }
---
+++ src/schedulers/inline_scheduler.rs
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
+
+use crate::{OperationState, Receiver, Scheduler, Sender};
+
+struct InlineOperationState<R>
+where
+    R: Receiver<Value = (), Error = ()>,
+{
+    pub receiver: R,
+}
+
+impl<R> OperationState for InlineOperationState<R>
+where
+    R: Receiver<Value = (), Error = ()>,
+{
+    fn start(&mut self) {
+        self.receiver.set_value(());
+    }
+}
+
+pub struct InlineSender {}
+
+impl Sender for InlineSender {
+    type Value = ();
+
+    type Error = ();
+
+    fn connect<R>(self, receiver: R) -> impl OperationState
+    where
+        R: Receiver<Value = Self::Value, Error = Self::Error>,
+    {
+        InlineOperationState { receiver }
+    }
+}
+
+pub struct InlineScheduler {}
+
+impl Scheduler for InlineScheduler {
+    fn schedule(&mut self) -> impl Sender {
+        InlineSender {}
+    }
+}
