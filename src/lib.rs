0e2c83426992153848d7be242259373178a6778c
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-02-24 12:31:36 +0000

    make doc in correct place

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
+#[doc(hidden)]
 pub mod core;
+#[doc(inline)]
 pub use core::*;
 
 pub mod schedulers;
--- src/schedulers.rs
+++ src/schedulers.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 Rishabh Dwivedi (rishabhdwivedi17@gmail.com)
 
+#[doc(hidden)]
 pub mod inline_scheduler;
+#[doc(inline)]
 pub use inline_scheduler::*;
